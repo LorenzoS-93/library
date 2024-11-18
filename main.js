6b16924b27c3087f65f67f7279838025db27e89f
LorenzoS-93 <180427301+LorenzoS-93@users.noreply.github.com>
2024-11-18 11:11:31 +0100

    finished the table version

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ function Book(title, author, pages, read) {
     this.read=read;
 }
 
+function openAddModal() {
+    dialog.showModal();
+}
+
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read");
 const book2 = new Book("The Lies of Locke Lamora", "Scott Lynch", "605", "not read");
 const book3 = new Book("A Wizard of Earthsea", "Ursula K. Le Guin", "205", "not read");
@@ -40,11 +44,19 @@ function addBookToLibrary() {
     showBook();
 }
 
+function changeReadStatus(book) {
+    if(book.read === "read")
+        book.read = "not read";
+    else
+        book.read = "read";
+}
+
 function showBook() {
     // Clear the table
     while (tbody.firstChild)
         tbody.removeChild(tbody.lastChild);
     // Loop the array and add each attribute of the books to a cell
+    let index = 0;
     for(const e of myLibrary) {
         const tableRow = document.createElement("tr");
         tableRow.setAttribute("class", "row");
@@ -53,12 +65,29 @@ function showBook() {
             rowCell.textContent = e[att];
             tableRow.appendChild(rowCell);
         }
+        const rowCell = document.createElement("td");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "delete";
+        deleteBtn.setAttribute("class", index);
+        deleteBtn.addEventListener("click", () => {
+            deleteBtn.parentNode.parentNode.remove();
+            const i = deleteBtn.getAttribute("class");
+            myLibrary.splice(i, 1);
+        });
+        rowCell.appendChild(deleteBtn);
+        const readBtn = document.createElement("button");
+        readBtn.textContent = "read";
+        readBtn.setAttribute("class", index);
+        readBtn.addEventListener("click", () => {
+            const i = readBtn.getAttribute("class");
+            changeReadStatus(myLibrary[i]);
+            showBook();
+        });
+        rowCell.appendChild(readBtn);
+        tableRow.appendChild(rowCell);
         tbody.appendChild(tableRow);
+        index++;
     }
-}
-
-function openAddModal() {
-    dialog.showModal();
 }
 
 showBook();
