76a3f05a1764c652f10c9c63cece46ad487927aa
LorenzoS-93 <180427301+LorenzoS-93@users.noreply.github.com>
2024-11-26 17:05:11 +0100

    card addiction fuction compleated

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const dialog = document.querySelector("dialog");
 const openDialogBtn = document.querySelector("#open-dialog");
 const closeDialogBtn = document.querySelector("#close-dialog");
 const addBookBtn = document.querySelector("#add-book");
-const main = document.querySelector("main")
+const main = document.querySelector("main");
 
 /* Initialize  myLibrary array */
 const myLibrary = [];
@@ -26,14 +26,51 @@ const closeDialog = () => {
     dialog.close();
 };
 
-const addCard = () => {
-    const newCard = document.createElement("div");
-    newCard.setAttribute("class", "card");
-    main.insertBefore(newCard, openDialogBtn);
-}
+const addCard = (e) => {
+    
+    main.insertBefore(e, openDialogBtn);
+};
+
+const fillCard = (e) => {
+    const bookCover = document.createElement("div");
+    bookCover.setAttribute("class", "image-placeholder");
+    const bookAttributes = document.createElement("div");
+    bookAttributes.setAttribute("class", "attributes");
+    const bookTitle = document.createElement("p");
+    bookTitle.setAttribute("class", "title");
+    const bookAuthor = document.createElement("p");
+    bookAuthor.setAttribute("class", "author");
+    const bookPages = document.createElement("p");
+    bookPages.setAttribute("class", "pages");
+    const bookSummary = document.createElement("p");
+    bookSummary.setAttribute("class", "summary");
+
+    const cover = "img";
+    const title = "title";
+    const author = "author";
+    const pages = "#pages";
+    const summary = "summary";
+
+    bookCover.textContent = cover;
+    e.appendChild(bookCover);
+    e.appendChild(bookAttributes);
+    bookTitle.textContent = "Title: " + title;
+    bookAttributes.appendChild(bookTitle);
+    bookAuthor.textContent = "Author: " + title;
+    bookAttributes.appendChild(bookAuthor);
+    bookPages.textContent = "Pages: " + title;
+    bookAttributes.appendChild(bookPages);
+    bookSummary.textContent = "Summary: " + summary;
+    e.appendChild(bookSummary);
+
+};
 
 const addBook = () => {
-    addCard();
+    const newCard = document.createElement("div");
+    newCard.setAttribute("class", "card");
+
+    addCard(newCard);
+    fillCard(newCard);
     dialog.close();
 };
 
